a8abcdca3378f5bea42197e92b9cf531076f68d1
Neil Lal <neillal1992@gmail.com>
2021-11-14 14:05:44 -0800

    first commit

---
+++ Assets/Scripts/DespawnOnHeight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DespawnOnHeight : MonoBehaviour {
+
+	private GameObject audioObj;
+
+	// Use this for initialization
+	void Start () {
+
+		// Hold audio object to destroy
+		audioObj = GameObject.FindGameObjectsWithTag("Audio")[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (transform.position.y < -5) {
+			// Player died, Game over steps
+			Destroy(audioObj);
+			MazeCounter.Reset();
+			SceneManager.LoadScene("GameOver");
+		}
+	}
+}
---
+++ Assets/Scripts/MazeCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MazeCounter : MonoBehaviour {
+
+	public static int level = 0;
+	private static Text txt;
+
+	// Use this for initialization
+	void Start () {
+
+		// Increment on call
+		level++;
+
+		// Change Level text
+		txt = gameObject.GetComponent<Text>();
+		txt.text = string.Concat("Level: ", level);
+	}
+
+	public static void Reset() {
+
+		// Reset the level counter
+		level = 0;
+	}
+}
